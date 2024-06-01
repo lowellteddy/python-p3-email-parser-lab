8bef95c18808cc4e1b676e706f8cf468e999ccaa
lowellteddy <lowellteddy88@gmail.com>
2024-06-01 17:00:24 +0300

    completed lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    pattern = re.compile (r'([a-zA-Z0-9+._-]+@[a-zA-Z0-9+._-]+\.[a-zA-Z0-9+._-]+)')
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+
+    def parse(self):
+        email = re.split(r',|\s', self.email_address)
+        parsed_email = set()
+        for i in email:
+            if self.pattern.fullmatch(i):
+                parsed_email.add(i)
+        return sorted(list(parsed_email))
